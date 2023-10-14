0d9c2409ebfdda9fe9473f3eaaa4638d5c7dcbf8
SandhyaSriArla <sandhyasri.21bce9265@vitapstudent.ac.in>
2023-10-14 21:04:55 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm constantly open to new chances and collaborations and eager to learn new things. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="mailto:arlasandhyasri2200@gmail.com">Email</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/ArlaSandhyaSri " target="_blank" rel="noopener noreferrer">github</a>
+        </li>
+       
+      </ul>
+      <p>
+      Connecting with you is something I'm excited about!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/Projects.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+	  <h5>MERN</h5>
+              
+              <a
+                href="https://github.com/ArlaSandhyaSri/MERN_TASK_2ement-system"
+                
+              >
+                GitHub Repository
+              </a>
+      {/* Add your project components or details here */}
+    </section>
+  );
+}
+
+export default Projects;
